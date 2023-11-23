89fe3e3f2d3af4f372f3ef582e27558e47261d82
BRIGHT <wissen30@hotmail.com>
2023-11-23 21:48:09 +0300

    Add project files.

---
+++ WebApi2/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2.Controllers
+{
+    public class User
+    {
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Token { get; set; }
+
+
+    }
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new User()
+            {
+
+                Id = 1,
+                Name = "test",
+                Token = new Guid().ToString()
+
+
+
+            });
+        }
+        [HttpPost]
+        public ActionResult Post(User user)
+        {
+            return Ok("Gönderilen değer"+user.Name);
+        }
+    }
+}
